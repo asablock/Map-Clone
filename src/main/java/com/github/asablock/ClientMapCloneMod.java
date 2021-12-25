1b44be2344088362ba0b03dbd067d5712faae832
asablock <005cga@163.com>
2021-12-25 08:32:42 +0800

    Initial commit

---
+++ src/main/java/com/github/asablock/ClientMapCloneMod.java
@@ -0,0 +1,10 @@
+package com.github.asablock;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ClientMapCloneMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+	}
+}
